cb7ab46ee7f86f9dd06c1b297c305d1edabddb43
SRIKANTHSK27 <117260783+SRIKANTHSK27@users.noreply.github.com>
2023-02-24 12:21:11 +0530

    rest api

---
+++ REST API/d4students/src/main/java/com/example/demo/D4studentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D4studentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D4studentsApplication.class, args);
+	}
+
+}
