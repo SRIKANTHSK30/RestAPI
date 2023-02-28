9cd4216dea971de1f940f9a43e84e6d357f3cb66
SRIKANTHSK27 <117260783+SRIKANTHSK27@users.noreply.github.com>
2023-02-28 20:36:18 +0530

    RestApi

---
+++ REST API/d7-Queries/src/main/java/com/example/demo/D7QueriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D7QueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D7QueriesApplication.class, args);
+	}
+
+}
